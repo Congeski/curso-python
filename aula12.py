37af58bfed46e573e57f9d560a39ff01fc90a07b
vinicius.congeski <viniciuscongelo@gmail.com>
2023-04-10 23:03:03 -0300

    Exercicio calculo IMC, operadores aritmeticos e concatenacao

---
+++ aula12.py
@@ -0,0 +1,12 @@
+nome = 'Vinicius Congeski'
+altura = 1.84
+peso = 120
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+
+# Vinicius Congeski tem 1.84 de altura,
+# pesa 120 quilos e seu IMC é
+# 35.44423440453686

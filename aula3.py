813eed670b8b0fd2f83e90f590636af2148fc852
vinicius.congeski <viniciuscongelo@gmail.com>
2023-04-10 22:55:38 -0300

    Iniciando o projeto com as aulas de python

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Vinicius Congeski')
+print(1, 'Vinicius "Congeski"')
+
+# Aspas duplas
+print(2, "Vinicius 'Congeski'")
+
+# Escape
+print("Vinicius \"Congeski\"")
+
+# r
+print(r"Vinicius \"Congeski\"")

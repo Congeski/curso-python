f8fcb05af32b10c086d5e4724d91ad88d2d0bec8
Vinicius Congeski <viniciuscongelo@gmail.com>
2023-06-14 23:16:29 -0300

    while - Qual letra apareceu mais vezes na frase?

---
+++ aula42.py
@@ -0,0 +1,29 @@
+""" / serve para quebrar linhas"""
+frase = 'O Python é uma linguagem de programação ' \
+    'multiparadigma. ' \
+    'Python foi criado pro Guido van Rossum.'
+
+#  print(frase.count('Python'))  # Quantas vezes a palavra Python aparece na frase, a função count faz a contagem;
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
+while i < len(frase):
+    letra_atual = frase[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes <= qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print('A letra que apareceu mais vezes foi '
+      f'"{letra_apareceu_mais_vezes}" que apareceu '
+      f'{qtd_apareceu_mais_vezes}x'
+    )
